350b97768c6b89c74988e490e20e899929bb3b19
Felipe Salles <prof.felipe.salles@gmail.com>
2020-03-16 13:50:57 -0300

    Exibindo informações de perfil

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { StorageService } from '../../services/storage.service';
+import { ClienteDTO } from '../../models/cliente.dto';
+import { ClienteService } from '../../services/domain/cliente.service';
 
-/**
- * Generated class for the ProfilePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+
 
 @IonicPage()
 @Component({
@@ -16,19 +13,23 @@ import { StorageService } from '../../services/storage.service';
 })
 export class ProfilePage {
 
-  email:string;
+  cliente:ClienteDTO;
 
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
-    public storage:StorageService
+    public storage:StorageService,
+    public clienteService:ClienteService
     ) {
   }
 
   ionViewDidLoad() {
     let localUser = this.storage.getLocalUser();
     if(localUser && localUser.email){
-      this.email = localUser.email;
+      this.clienteService.findByEmail(localUser.email).subscribe(response =>{
+        this.cliente = response;
+      }, 
+       error => {});
     }
   }
 
